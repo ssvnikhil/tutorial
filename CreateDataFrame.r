25e139d424bbbb3e937052955d703c474b396c29
Nikhil <ssvnikhil03@gmail.com>
2017-09-02 12:15:53 -0500

    Access individual columns, rows and cells.

--- CreateDataFrame.r
+++ CreateDataFrame.r
@@ -49,3 +49,39 @@ tail(theDF)
 tail(theDF, n =9)
 
 class(theDF)
+
+# Access individual column from DataFrame
+
+theDF$Sport 
+theDF[3,2]  # Accessing individual cell
+
+theDF[3,2:3] # Accessing a particular row elements
+
+theDF[c(3,5),2] # 3rd and 5th row of 2nd column elements 
+
+theDF[,3] # Values are displayed in Vector format
+theDF[,2:3] # Values are displayed in DataFrame format
+
+class(theDF[,3])
+theDF[,3,drop=FALSE] # don't drop into a vector
+
+class(theDF[,3,drop=FALSE])
+
+theDF[2,]
+class(theDF[2,])
+
+# Note: To show the values in a vector all the components in it must be of same type.
+
+
+theDF[2:4,]
+
+theDF[c("First","Sport")]
+theDF[c("Sport","First")] # Order is not necessary!
+theDF[,"Sport"]
+theDF[,"Sport",drop=FALSE] # DataFrame format
+theDF["Sport"]
+
+theDF[["Sport"]] # Double square brackets show the values in Vector format.
+theDF[["First"]]
+
+# Data Frame can store numeric, character, factor, integer in all data sets.
