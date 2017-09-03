b2558a268d468724f845b7a58a0a562b9dec94cf
Nikhil <ssvnikhil03@gmail.com>
2017-09-03 14:33:15 -0500

    Learned about Histograms

---
+++ HistogramCarat.r
@@ -0,0 +1,5 @@
+# Histograms are the best form of Visualization
+# Plots the relative frequency of each bucket
+hist(diamonds$carat) # plots the carat data in bar diagram
+hist(diamonds$carat, main = "Carat histogram", xlab = "carat")
+# Changed heading to Carat histogram and x co-ordinate to carat.
