b67a08305415d483a6eb16936ded23313e0e2ae0
Nikhil <ssvnikhil03@gmail.com>
2017-09-03 14:58:09 -0500

    Understand the usage of boxplot. Highly important concept in Statistical inference.

---
+++ BoxPlot.r
@@ -0,0 +1,8 @@
+# boxplot is against one variable and will tell us how the data is spread out.
+# Inside the box it represents 50% of the data.
+# upper line has 3rd quartile and lower line has 1st quartile.
+# middle line has median 
+# inter quartile range = (3rd quartile - 1st quartile)
+
+boxplot(diamonds$carat)
+
