ccbc011462af812c8084624266521fe3a514c9f0
Nikhil <ssvnikhil03@gmail.com>
2017-09-02 19:36:06 -0500

    Load packages and data types required in it.

---
+++ LoadDataIncludedInR.r
@@ -0,0 +1,9 @@
+require(ggplot2)
+data(diamonds)
+head(diamonds)
+
+data("tips",package = "reshape2")
+head(tips)
+
+data() # See a list of data available in R and even shows you what type of data is present in package
+
