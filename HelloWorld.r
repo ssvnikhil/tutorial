081c5aa3373f8e8028609aa39c3f0cbb154e34b5
Nikhil <ssvnikhil03@gmail.com>
2017-09-04 15:16:13 -0500

    Created a function to print HelloWorld.

---
+++ HelloWorld.r
@@ -0,0 +1,10 @@
+# Function is defined to a variable
+
+say.hello <- function()
+{
+    print("Hello,World!")
+}
+
+say.hello # This displays the whole function code
+
+say.hello() # executes the code in the function
