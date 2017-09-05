0111b1ff8220438bc058fb528614e16310eb0eb4
Nikhil <ssvnikhil03@gmail.com>
2017-09-04 23:17:08 -0500

    Usage of if statement.

---
+++ IfStatement.r
@@ -0,0 +1,18 @@
+1 == 1
+1 < 1
+1 <= 1
+1 > 1
+1 >= 1
+1 != 1
+
+toCheck <- 1
+if(toCheck == 1)
+{
+    print("Hello, if statement is working")
+}
+
+toCheck <- 0
+if(toCheck == 1)
+{
+    print("It will not work")
+}
