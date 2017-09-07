7e9d6363011d71f10a58ee5f9e2db4e3ed17ee02
Nikhil <ssvnikhil03@gmail.com>
2017-09-07 17:51:15 -0500

    Learnt about paste and sprintf functions.

---
+++ CombineStrings.r
@@ -0,0 +1,23 @@
+paste("Hello","Jared","and others")
+paste("Hello","Jared","and others", sep = "/")
+
+paste(c("Hello","Hey","Howdy"), c("Jared","Bob","David"))
+# It pastes with the corresponding elements
+
+paste("hello", c("Jared","Bob","David"))
+paste("Hello", c("Jared","Bob","David"), c("Goodbye", "Seeya"))
+
+vectorOfText <- c("Hello", "everyone", "out there", ".")
+paste(vectorOfText, collapse=" ")
+
+
+person <- "Jared"
+partySize <- 8
+waitTime <- 25
+
+paste("Hello ", person, ", your party of ",
+      partySize," will be seated in ", waitTime, " minutes.", sep ="")
+
+sprintf("Hello %s, your party of %s will be seated in %s minutes.", 
+        person, partySize, waitTime)
+
